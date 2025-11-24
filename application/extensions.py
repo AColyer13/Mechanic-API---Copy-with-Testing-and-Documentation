05d34d5b2e16f14bcea0a41689b21604d472ae4f
Adam Colyer <adamcolyer@gmail.com>
2025-11-23 22:40:45 -0600

    flask limiter fix

--- application/extensions.py
+++ application/extensions.py
@@ -18,7 +18,8 @@ ma = Marshmallow()
 migrate = Migrate()
 limiter = Limiter(
     key_func=get_remote_address,
-    default_limits=["200 per day", "50 per hour"]
+    default_limits=["200 per day", "50 per hour"],
+    storage_uri="memory://"  # Explicitly set storage to suppress warning
 )
 cache = Cache()
 
